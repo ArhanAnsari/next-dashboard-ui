046980e41db8c57c1c129e7e8aed20764ddb0cc2
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-18 19:44:17 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CodeWithArhan School Management Dashboard",
+  title: "Arhan School Management Dashboard",
   description: "Next.js School Management System",
 };
 
